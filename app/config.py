edf3f20f7519ac6dbf845fcdff1e24462cdecc1f
JoseCPJ <j.quinonez0810@gmail.com>
2024-12-20 19:45:08 -0500

    .

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ import sys
 current_dir = os.getcwd()
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)  # Inserta el directorio actual al inicio de sys.path
-print(f"PYTHONPATH configurado a: {current_dir}")
+print(f"\n\nPYTHONPATH configurado a: {current_dir}")
 
 print("\n\nScript principal ejecutándose...\n\n")
