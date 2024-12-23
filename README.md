652ee53fe32358f0c9d016dd816ccf8575153e38
JoseCPJ <j.quinonez0810@gmail.com>
2024-12-22 21:52:30 -0500

    beforestart

--- README.md
+++
@@ -1,2 +0,0 @@
-# Gymstats
-Aplicación web para planificar y seguir entrenamientos de gimnasio. Frontend en HTML, CSS y JavaScript con diseño responsivo y tema oscuro. Backend  en FastAPI. Personaliza rutinas, registra progresos y visualiza estadísticas para optimizar tu fitness.
--- validation/const.py
+++
@@ -1,9 +0,0 @@
-import os
-# Carpeta para almacenar los archivos CSV
-DB_FOLDER = "db/dB_temp"
-
-# Rutas de los archivos CSV
-USERS_FILE = os.path.join(DB_FOLDER, "users.csv")
-PLANIFICATIONS_FILE = os.path.join(DB_FOLDER, "planifications.csv")
-PLANIFICATION_CONTENT_FILE = os.path.join(DB_FOLDER, "planification_content.csv")
-EXERCISES_FILE = os.path.join(DB_FOLDER, "exercises.csv")
