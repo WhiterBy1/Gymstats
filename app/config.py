6bf373f04db22d17bdfd5c8177ad4120dd8d032f
JoseCPJ <j.quinonez0810@gmail.com>
2024-12-30 12:55:35 -0500

    Project distribution init

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-#configuracion
-
-current_dir = os.getcwd()
-if current_dir not in sys.path:
-    sys.path.insert(0, current_dir)  # Inserta el directorio actual al inicio de sys.path
-    print(f"\n\nPYTHONPATH configurado a: {current_dir}\n\n")
--- main.py
+++
@@ -1,5 +0,0 @@
-import app.config
-from app.menu import mainmenu
-
-if __name__ == "__main__":   
-    mainmenu()
